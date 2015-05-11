e4a781887cf50af042a0e270d485a682c68d932b
Will Hines <whines@gmail.com>
2015-05-10 20:37:08 -0700

    trying to fix texting

--- common.php
+++ common.php
@@ -751,7 +751,7 @@ function wbh_shorten_link($link) {
 	// under whines@gmail.com / meet1962
 	
 	//tempoary while working locally
-	$link = preg_replace('/localhost:8888/', 'www.willhines.net', $link);
+	//$link = preg_replace('/localhost:8888/', 'www.willhines.net', $link);
 	
 	$link = urlencode($link);
 	$response = file_get_contents("https://api-ssl.bitly.com/v3/shorten?access_token=70cc52665d5f7df5eaeb2dcee5f1cdba14f5ec94&longUrl={$link}&format=txt");
