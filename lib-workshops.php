111a356a32e22dc5e6842eb893833dc1bd312099
Will Hines <whines@gmail.com>
2017-11-15 01:08:05 -0800

    added dark table headings

--- lib-workshops.php
+++ lib-workshops.php
@@ -174,7 +174,7 @@ function get_workshops_list($admin = 0, $page = 1) {
 	
 	$body = $paginator->createLinks();
 	
-	$body .= '<table class="table table-striped table-bordered"><thead>
+	$body .= '<table class="table table-striped table-bordered"><thead class="thead-dark">
 		<tr>
 			<th class="workshop-name" scope="col"><span class="oi oi-people" title="people" aria-hidden="true"></span> Workshop</th>
 			<th scope="col"><span class="oi oi-calendar" title="calendar" aria-hidden="true"></span> When</th>
