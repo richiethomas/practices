81e0e2d2962db4a41aa68809d47d13dfd424a928
Will Hines <whines@gmail.com>
2017-12-10 11:22:19 -0800

    delete mail log button

--- admin_mail_log.php
+++ admin_mail_log.php
@@ -4,12 +4,19 @@ $heading = "practices: admin";
 include 'lib-master.php';
 include 'libs/validate.php';
 
+Wbhkit\set_vars(array('ac'));
+
+
+if ($ac == 'condel') {
+	file_put_contents(MAIL_LOG, '');
+}
 
 $log = file_get_contents(MAIL_LOG);
 if (!$log) {
 	$log = 'No email activity!';
 }
 $view->data['log'] = $log;
+$view->data['ac'] = $ac;
 $view->renderPage('admin_mail_log');
 
 
--- views/admin_mail_log.php
+++ views/admin_mail_log.php
@@ -1,4 +1,7 @@
-<h2>Email Activity</h2>
+<?php if ($ac == 'del') { ?>
+<div class='alert alert-danger'><p>Really <a class='btn btn-outline-danger' href='<?php echo $sc;?>?ac=condel'>delete the mail log</a>?</p></div>
+<?php } ?>
+<h2>Email Activity <small><a class='btn btn-outline-danger' href='<?php echo $sc; ?>?ac=del'>delete log</a></small></h2>
 <pre>
 <?php echo $log; ?>
 </pre>
