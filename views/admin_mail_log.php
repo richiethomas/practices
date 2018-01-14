8caf2fb9ae0bf866a5b21dceec0458c3aaa16a93
Will Hines <whines@gmail.com>
2018-01-13 17:00:04 -0800

    tweak error log html

--- views/admin_mail_log.php
+++ views/admin_mail_log.php
@@ -1,7 +1,7 @@
 <?php if ($ac == 'del') { ?>
-<div class='alert alert-danger'><p>Really <a class='btn btn-outline-danger' href='<?php echo $sc;?>?ac=condel'>delete the mail log</a>?</p></div>
+<div class='alert alert-danger'><p>Really <a class='btn btn-outline-danger' href='<?php echo $sc;?>?ac=condel'>delete the error log</a>?</p></div>
 <?php } ?>
-<h2>Email Activity <small><a class='btn btn-outline-danger' href='<?php echo $sc; ?>?ac=del'>delete log</a></small></h2>
+<h2>Error Log <small><a class='btn btn-outline-danger' href='<?php echo $sc; ?>?ac=del'>delete log</a></small></h2>
 <pre>
 <?php echo $log; ?>
 </pre>
