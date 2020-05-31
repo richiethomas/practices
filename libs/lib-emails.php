72ceb19b009d2c6bdc57d06be27b46f3a08a4baa
Will Hines <whines@gmail.com>
2020-05-31 11:02:30 -0700

    email once again sent with mail function

--- libs/lib-emails.php
+++ libs/lib-emails.php
@@ -26,15 +26,14 @@ function centralized_email($to, $sub, $body) {
 	  $headers['Content-Type'] = "text/html";
 	  $headers['charset'] = "ISO-8859-1";
 
-  	 $sent = $smtp->send($to, $headers, $body);  // laptop can use the SMTP server on willhines.net
-	 /*
 	  if (LOCAL) {
+		 $sent = $smtp->send($to, $headers, $body);  // laptop can use the SMTP server on willhines.net
  	  } else {
+		  // live server CAN use SMTP but it's slower and no better!
 		  unset($headers['Subject']);
 		  unset($headers['To']);
 	  	 $sent = mail($to, $sub, $body, $headers); // willhinesimprov.com uses local server
  	  }
-	 */
 	
 	$ts = date("Y-m-d H:i:s").' ';
 	$mail_activity = "emailed '$to', '$sub'\n";
