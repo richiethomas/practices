7b5845f92c282245f9dfb037ec82ead2a22bad81
Will Hines <whines@gmail.com>
2022-11-23 00:41:47 -0500

    simplify front page (big change)

--- views/classes.php
+++ views/classes.php
@@ -1,23 +1,8 @@
 <h1 class="page-title">Classes</h1>
-<h5 class='mb-5'>compact view
-	<span class='text-muted'><small>(see <?php
-if ($mode == 'text') {
-	echo "<a href='/classes'>just regular compact</a>";
-} else {
-	echo "<a href='/classes/view/text'>very compact</a>";
-}
-?>
-)</small></span></h5>
-
 	<div class="row justify-content-center">
 		<div class="col-md-12">
 			
 <?php		
-
-echo "<div>Times shown in ({$u->fields['time_zone_friendly']})</div>";
-
-$ip_html = '';
-$ol_html = '';
 
 $ip_text = '';
 $ol_text = '';
@@ -29,42 +14,24 @@ foreach ($upcoming_workshops as $wk) {
 	}
 	
 	$wk = prep_wk($wk);
-	
-	$row_html = "<div class='row mt-4'>\n";
-	$row_html .= "<div class='col-md-4'><a href='/workshop/view/{$wk->fields['id']}'>{$wk->fields['title']}</a>";
-	
-	if ($wk->fields['soldout']) {
-		$row_html .= " - <span class='text-danger'>Sold Out</span>";
-	}
-	
-	
-	if ($u->check_user_level(2)) { 
-		$row_html .= "<br><span class='text-muted'><small>({$wk->fields['enrolled']} / {$wk->fields['capacity']})</small></span>\n";
-	}
-	
-	$row_html .= "</div>\n";
-	$row_html .= "<div class='col-md-3'>{$wk->fields['teacher_name']}</div>\n";
-	$row_html .= "<div class='col-md-2'>{$wk->fields['classpage_start']}</div>\n";
 
-	$row_html .= "<div class='col-md-3'>{$wk->fields['total_sessions']} ".($wk->fields['total_sessions'] == 1 ? 'session': 'sessions').", {$wk->fields['costdisplay']}</div>\n";
-	
-	$row_html .= "</div>\n";
-	
-	
-	// text view
-	$text_html = "<p class='m-1 p-0 fs-6 lh-base'><a class='text-decoration-none' href='/workshop/view/{$wk->fields['id']}'>{$wk->fields['title']}</a>, {$wk->fields['teacher_name']}, {$wk->fields['classpage_start']}, {$wk->fields['total_sessions']} ".($wk->fields['total_sessions'] == 1 ? 'week': 'weeks').", {$wk->fields['costdisplay']}".($wk->fields['soldout'] ? " - <span class='text-danger'>Sold Out</span>" : '');
+	$text_html = "<div class='row mb-3'>
+		<div class='col-3' ><a href='/workshop/view/{$wk->fields['id']}'>{$wk->fields['title']}</a><br><span class='text-muted'><small>{$wk->fields['total_sessions']} ".($wk->fields['total_sessions'] == 1 ? 'week': 'weeks')."</small></span></div>
+		<div class='col-3' >{$wk->fields['teacher_name']}</div>
+		<div class='col-3' >{$wk->fields['classpage_start']}</div>
+		<div class='col-3'>{$wk->fields['costdisplay']}".($wk->fields['soldout'] ? " - <span class='text-danger'>Sold Out</span>" : '');
 	
 	if ($u->check_user_level(2)) { 
 		$text_html .= " <span class='text-muted'><small>({$wk->fields['enrolled']} / {$wk->fields['capacity']})</small></span>\n";
 	}
 	
-	$text_html .= "</p>\n";
+	$text_html .= "</div></div>\n";
+
+
 	
 	if (in_array('inperson', $wk->fields['tags_array'])) {
-		$ip_html .= $row_html;
 		$ip_text .= $text_html;
 	} else {
-		$ol_html .= $row_html;
 		$ol_text .= $text_html;
 	}
 }	
@@ -106,15 +73,15 @@ foreach ($unavailable_workshops as $wk) {
 } 
 
 echo "<h2 class='my-3'>Open Online Classes</h2>\n";
-echo $mode == 'text' ?
-	($ol_text ? $ol_text : '<p>No multi-week courses coming up!</p>') :
-	($ol_html ? $ol_html : '<p>No multi-week courses coming up!</p>');
+echo "<div class='row mb-2'><div class='col-6'>&nbsp;</div><div class='col-3 fw-bold'>Times in (".$u->fields['time_zone_friendly'].")</div><div class='col-3'>&nbsp;</div></div>";
+
+echo $ol_text ? $ol_text : '<p>No multi-week courses coming up!</p>';
 	
 
 echo "<h2 class='my-3'>Open In Person Los Angeles Classes</h2>\n";
-echo $mode == 'text' ?
-	($ip_text ? $ip_text : '<p>No multi-week courses coming up!</p>') :
-	($ip_html ? $ip_html : '<p>No multi-week courses coming up!</p>');
+echo "<div class='row mb-2'><div class='col-6'>&nbsp;</div><div class='col-3 fw-bold'>Times in (".$u->fields['time_zone_friendly'].")</div><div class='col-3'>&nbsp;</div></div>";
+
+echo $ip_text ? $ip_text : '<p>No multi-week courses coming up!</p>';
 
 
 if ($upc_html) {
@@ -126,7 +93,7 @@ if ($upc_html) {
 
 function prep_wk($wk) {
 	global $u;
-	$wk->fields['classpage_start'] = date("D M j", strtotime($wk->fields['start_tz'])).' '.Wbhkit\friendly_time($wk->fields['start_tz']).' ('.$u->fields['time_zone_friendly'].')';
+	$wk->fields['classpage_start'] = date("D M j", strtotime($wk->fields['start_tz'])).' '.Wbhkit\friendly_time($wk->fields['start_tz']);
 	
 	if ($wk->fields['costdisplay'] == 'Pay what you can') { $wk->fields['costdisplay'] = 'donation'; }
 	
